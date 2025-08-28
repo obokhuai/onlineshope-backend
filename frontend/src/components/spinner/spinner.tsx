e77200a21fcd80d30883f8236682ef61a77d6445
Obokhuai <ehitheo@gmail.com>
2025-08-28 09:31:42 +0200

    code moved to backend

--- frontend/src/components/spinner/spinner.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import './spinner.css';
-
-const Loader: React.FC = () => (
-  <div className="custom-spinner" role="status" aria-label="Loading"></div>
-);
-
-export default Loader;
